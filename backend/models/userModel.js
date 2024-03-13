89eb5656f1438c6a1716186757ce6be1e83438c9
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-13 00:00:57 +0000

    Add password encryption, token generation, and password reset functionality to UserModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,6 +41,18 @@ const userSchema = new mongoose.Schema({
         type: String,
         trim: true,
     },
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+        }
+    ],
+    saved: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+        }
+    ],
     followers: [
         {
             type: mongoose.Schema.Types.ObjectId,
@@ -54,3 +68,32 @@ const userSchema = new mongoose.Schema({
     resetPasswordToken: String,
     resetPasswordExpiry: Date,
 });
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.methods.generateToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+}
+
+userSchema.methods.getResetPasswordToken = async function () {
+
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+    this.resetPasswordExpiry = Date.now() + 15 * 60 * 1000;
+
+    return resetToken;
+}
+
+module.exports = mongoose.model("User", userSchema);
