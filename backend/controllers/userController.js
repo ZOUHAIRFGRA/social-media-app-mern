a62675d4dbc496c41bac464b267bf5137194d0e4
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-18 00:26:17 +0000

    Add user signup functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,43 @@
-// this will have user logic
+const User = require('../models/userModel');
+const catchAsync = require('../middlewares/catchAsync');
+const sendCookie = require('../utils/sendCookie');
+const ErrorHandler = require('../utils/errorHandler');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
+const cloudinary = require('cloudinary');
+
+// Signup User
+exports.signupUser = catchAsync(async (req, res, next) => {
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "instagram/avatars",
+        width: 150,
+        crop: "scale",
+    });
+
+    const { name, email, username, password } = req.body;
+
+    const user = await User.findOne({
+        $or: [{ email }, { username }]
+    });
+    if (user) {
+        if (user.username === username) {
+            return next(new ErrorHandler("Username already exists", 401));
+        }
+        return next(new ErrorHandler("Email already exists", 401));
+    }
+
+    const newUser = await User.create({
+        name,
+        email,
+        username,
+        password,
+        avatar: {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+        },
+    })
+
+    sendCookie(newUser, 201, res);
+});
+
