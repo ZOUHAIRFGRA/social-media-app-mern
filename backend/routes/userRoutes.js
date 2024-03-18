428f3762876b4ac857d4512f31cef0cb08219a91
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-18 00:28:49 +0000

    Add user routes and controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,9 @@
-//  routes
+const express = require('express');
+const { loginUser, signupUser, logoutUser} = require('../controllers/userController');
+const { isAuthenticated } = require('../middlewares/auth');
+
+const router = express();
+
+router.route("/signup").post(signupUser);
+router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
