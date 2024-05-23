7a370759858b20ca7677d45319ac41e05d3f4a45
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-05-23 13:12:54 +0100

    backend commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const MONGO_URI='mongodb+srv://zouhairfgra:root@cluster0.nuy3tte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const connectDatabase = () => {
+    mongoose.set('strictQuery', true);
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Mongoose Connected");
