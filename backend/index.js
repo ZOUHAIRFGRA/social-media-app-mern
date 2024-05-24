c911a0e280babc26aca6caebe314055335cc1bc7
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-05-24 13:16:25 +0100

    server => index

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
-const app = require("./app");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middlewares/error");
+const fileUpload = require("express-fileupload");
+
 const connectDatabase = require("./config/database");
 const cloudinary = require("cloudinary");
 const PORT = process.env.PORT || 4000;
 
 connectDatabase();
 app.get('/',(req,res)=>{
-  res.json('Server is running')
+  res.send('Server is running')
 })
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -70,3 +74,35 @@ io.on("connection", (socket) => {
     io.emit("getUsers", users);
   });
 });
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(fileUpload());
+app.use("/public", express.static("public"));
+
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+// import routes
+const post = require("./routes/postRoute");
+const user = require("./routes/userRoute");
+const chat = require("./routes/chatRoute");
+const message = require("./routes/messageRoute");
+
+app.use("/api/v1", post);
+app.use("/api/v1", user);
+app.use("/api/v1", chat);
+app.use("/api/v1", message);
+
+
+
+
+// error middleware
+app.use(errorMiddleware);
+
+module.exports = app;
